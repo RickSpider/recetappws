7ea4dd09b30a060fd4166d1a02e62e79580fcd9f
BlackSpider <rrgi89@hotmail.com>
2020-01-31 16:41:32 -0300

    primer commit

---
+++ src/main/java/com/doxa/recetapp/RecetappApplication.java
@@ -0,0 +1,13 @@
+package com.doxa.recetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetappApplication.class, args);
+	}
+
+}
