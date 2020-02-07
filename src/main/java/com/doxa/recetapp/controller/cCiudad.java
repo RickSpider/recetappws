9f75bc3d1fd7bcfbdfee8c29a9ec86a7443d1df7
BlackSpider <rrgi89@hotmail.com>
2020-02-06 22:05:19 -0300

    agregado controladores ciudad y titulo

---
+++ src/main/java/com/doxa/recetapp/controller/cCiudad.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.doxa.recetapp.controller;
+
+import com.doxa.recetapp.model.mCiudad;
+import com.doxa.recetapp.repository.rCiudad;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author BlackSpider
+ */
+@RestController
+@RequestMapping("/ciudad")
+public class cCiudad {
+    
+    @Autowired
+    private rCiudad rciudad;
+    
+    @GetMapping("/")
+    private Iterable<mCiudad> getCiudad(){
+    
+        return rciudad.findAll();
+        
+    }
+    
+}
---
+++ src/main/java/com/doxa/recetapp/controller/cTitulo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.doxa.recetapp.controller;
+
+import com.doxa.recetapp.model.mTitulo;
+import com.doxa.recetapp.repository.rTitulo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author BlackSpider
+ */
+@RestController
+@RequestMapping("/titulo")
+public class cTitulo {
+    
+    @Autowired
+    private rTitulo rtitulo;
+    
+    @GetMapping("/")
+    public Iterable<mTitulo> getTitulo(){
+    
+        return rtitulo.findAll();
+        
+    }
+    
+}
